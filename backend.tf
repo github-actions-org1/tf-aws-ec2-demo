645036a53853e22ba4b89b4998ef26980a915286
swilliams <solomon.fwilliams@gmail.com>
2023-12-30 14:55:55 -0600

    d

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-remote-backend-state-lab20231114133906936800000001"
-    key            = "terra-backend/terraform.tfstate"
-    encrypt        = true
-    region         = "us-east-2"
-    dynamodb_table = "dynamodb-state-locking"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "tf-remote-backend-state-lab20231114133906936800000001"
+#     key            = "terra-backend/terraform.tfstate"
+#     encrypt        = true
+#     region         = "us-east-2"
+#     dynamodb_table = "dynamodb-state-locking"
+#   }
+# }
