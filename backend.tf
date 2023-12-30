f40860b351378cc6d232f2a4711c84d608b2c662
swilliams <solomon.fwilliams@gmail.com>
2023-12-30 16:14:35 -0600

    d

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-remote-backend-state-lab20231114133906936800000001"
+    bucket         = "s3-remote-backend-20231224202330732000000001"
     key            = "terra-backend/terraform.tfstate"
     encrypt        = true
     region         = "us-east-2"
