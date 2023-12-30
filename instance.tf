dbb6f5d1b5a2780eb3d176268a734b3035a9cafc
swilliams <solomon.fwilliams@gmail.com>
2023-12-30 13:00:58 -0600

    new

--- instance.tf
+++ instance.tf
@@ -1,15 +1,15 @@
 # EC2 Instance
 resource "aws_instance" "ec2-demo" {
-  ami                    = data.aws_ami.amzlinux2.id
-  instance_type          = var.instance_type
-  user_data              = file("files/install-demo.sh")
-  key_name               = aws_key_pair.ssh_key.key_name
+  ami           = data.aws_ami.amzlinux2.id
+  instance_type = var.instance_type
+  user_data     = file("files/install-demo.sh")
+  key_name      = aws_key_pair.ssh_key.key_name
   # public_key = var.ssh_key_pair
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-demo.id]
   tags = {
-    Name = "ec2-demo"
+    Name        = "ec2-demo"
     Environment = "Developement"
     OS          = "RedHat Linux"
-    Managed     = "Terraform"    
+    Managed     = "Terraform"
   }
 }
